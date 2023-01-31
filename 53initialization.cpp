ffae7c7e283ecc30024ed1ad8150f6c7032249f5
Salon <rautsalon@gmail.com>
2023-01-31 21:30:14 +0545

    ololo

---
+++ 53initialization.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+/*
+Syntax for initialization list in constructor:
+constructor(argument-list): initialization-section
+{
+    assignment+other code;
+}
+*/
+class Test{
+    int a,b;
+    public:
+    // Test(int i,int j): a(i),b(j){
+    // Test(int i,int j): a(i),b(i+j){
+    // Test(int i,int j): a(i),b(2*j){
+    // Test(int i,int j): a(i),b(a+j){
+    // Test(int i,int j): b(j),a(i+b){  RED flag,this will create problemds because a will be initialized first
+    Test(int i,int j){
+        a=i;
+        b=j;
+        cout<<"Constructor executed"<<endl;
+        cout<<"Value of a is: "<<a<<endl;
+        cout<<"Value of b is: "<<b<<endl;
+    }
+};
+int main()
+{   
+    Test t(7,10);
+
+    return 0;
+}
